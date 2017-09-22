9ac803e8045afe3cbb5edb1059175d34edd80733
paul.houlston <paul.houlston@hotmail.co.uk>
2017-09-22 18:43:02 +0100

    Added failing test for scoring a spare

--- Tests.cs
+++ Tests.cs
@@ -55,6 +55,22 @@ namespace Tests
         }
     }
 
+    public class GIVEN_I_bowl_a_spare_in_the_first_frame
+    {
+        public class WHEN_I_knock_over_3_pins_on_my_first_throw_in_the_second_frame
+        {
+            [Fact]
+            public void THEN_my_score_is_16()
+            {
+                var game = new Game();
+                game.Roll(5);
+                game.Roll(5);
+                game.Roll(3);
+                Assert.Equal(16, game.Score());
+            }
+        }
+    }
+
     public class Game
     {
         private int _score;
