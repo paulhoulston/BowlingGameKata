3fa8830c8d3b5722f40dcf8cc813afb9d0fb5ed1
paul.houlston <paul.houlston@hotmail.co.uk>
2017-09-22 18:27:22 +0100

    Added failing test for zero pins knocked over on first roll in first frame

---
+++ Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace Tests
+{
+    public class GIVEN_I_am_bowling_my_first_ball_in_my_first_frame
+    {
+        public class WHEN_I_do_not_knock_over_any_pins
+        {
+            [Fact]
+            public void THEN_my_score_is_zero() 
+            {
+                var score = -1;
+                Assert.Equal(0, score);
+            }
+        }
+    }
+}
