e033ed4ff3e536bc9f2de6a32525996a52fc433f
paul.houlston <paul.houlston@hotmail.co.uk>
2017-09-22 18:38:20 +0100

    Added test to check gutter game case

--- Tests.cs
+++ Tests.cs
@@ -28,6 +28,22 @@ namespace Tests
         }
     }
 
+    public class GIVEN_I_bowl_10_frames
+    {
+        public class WHEN_I_do_not_knock_over_any_pins
+        {
+            [Fact]
+            public void THEN_my_score_is_zero()
+            {
+                var game = new Game();
+                for (var i = 0; i < 20; i++)
+                    game.Roll(0);
+                Assert.Equal(0, game.Score());
+
+            }
+        }
+    }
+
     public class Game
     {
         private int _score;
